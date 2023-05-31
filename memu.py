44ccafdb9633db47fe73852423ce055910d16462
Demakova Tatiana <demakova_ts@mail.ru>
2023-05-31 21:27:49 +0700

    Revert "create task7" commit 08121
    This reverts commit 08121f8a950c1288f37e9dc57a0056b2d93af9e9.

--- memu.py
+++ memu.py
@@ -7,20 +7,36 @@ def calc_memu():
         print("1-Сложение(+); \n2-Вычитание(-); \n3-Умножение(*); \n4-Деление(/); \n5-Выход ?")
         user_input = int(input())
         if(user_input == 1 ):
-            c = input_value()
+            print("ввведите первый аргумент")
+            arg1 = int(input())
+            print("ввведите второй аргумент")
+            arg2 = int(input())
+            c= Calculator(arg1, arg2)
             print(f"Результат равен: {c.__add__()}")
             print('')
         elif(user_input == 2):
-            c = input_value()
+            print("ввведите первый аргумент")
+            arg1 = int(input())
+            print("ввведите второй аргумент")
+            arg2 = int(input())
+            c= Calculator(arg1, arg2)
             print(f"Результат равен: {c.__sub__()}")
             print('')
         elif(user_input == 3):
-            c = input_value()
+            print("ввведите первый аргумент")
+            arg1 = int(input())
+            print("ввведите второй аргумент")
+            arg2 = int(input())
+            c= Calculator(arg1, arg2)
             print(f"Результат равен: {c.__mul__()}")
             print('')
         elif(user_input == 4): 
-            c = input_value()
-            if (c.arg2!=0):
+            print("ввведите первый аргумент")
+            arg1 = int(input())
+            print("ввведите второй аргумент")
+            arg2 = int(input())
+            if (arg2!=0):
+                c= Calculator(arg1, arg2)
                 print(f"Результат равен: {c.__div__()}")
             else: print("Деление на 0 не возможно")    
             print('')
@@ -29,11 +45,3 @@ def calc_memu():
             stop_work = True
         else:
             print("ВВедено некорректное значение")
-
-def input_value():
-    print("ввведите первый аргумент")
-    arg1 = int(input())
-    print("ввведите второй аргумент")
-    arg2 = int(input())
-    c= Calculator(arg1, arg2)
-    return c
